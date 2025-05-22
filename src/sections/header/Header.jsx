ed26ed6eac7877e1d89098adc590ad388769abfa
jonah odoh <jonahodoh@gmail.com>
2025-05-22 12:25:11 +0100

    updated pictures

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import HeaderImage from '../../assets/profile.jpg'
+import HeaderImage from '../../assets/profile2.jpg'
 import data from './data'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
